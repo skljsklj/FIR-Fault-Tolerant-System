d4f944285f69c2a59544d943a93fb11efd4e9aac
MilanSimin <simin.milan34@gmail.com>
2025-10-06 21:48:30 +0200

    mux stages fix and everything ok, i guess

--- hdl/mac_triplex_duplex.vhd
+++ hdl/mac_triplex_duplex.vhd
@@ -8,7 +8,7 @@ entity mac_triplex_duplex is
         fir_ord                         : natural := 20;      -- FIR order (taps = fir_ord+1)
         input_data_width                : natural := 24;
         -- number of pair-and-a-spare voter instances per TDR (e.g. 4 or 5)
-        number_of_voters_for_one_tdr    : natural := 4;
+        number_of_voters_for_one_tdr    : natural := 5;
         output_data_width               : natural := 24       -- default 2*input width
     );
     Port (
@@ -49,12 +49,15 @@ architecture Behavioral of mac_triplex_duplex is
 
     -- lines toward switch (data + 1 error bit)
     signal data_to_switch : switch_line_t(0 to total_num_of_voters-1) := (others => (others => '0'));
+    signal data_to_switch1 : switch_line_t(0 to total_num_of_voters-1) := (others => (others => '0'));
+    signal data_to_switch2 : switch_line_t(0 to total_num_of_voters-1) := (others => (others => '0'));
+    
 
     -- two selected lines
     -- per-TDR selector signals (two channels) sized by number_of_voters_for_one_tdr
     type sel_arr is array (0 to num_stages-1) of std_logic_vector(log2c(number_of_voters_for_one_tdr)-1 downto 0);
     signal sel_data_1 : sel_arr := (others => std_logic_vector(to_unsigned(0, log2c(number_of_voters_for_one_tdr))));
-    signal sel_data_2 : sel_arr := (others => std_logic_vector(to_unsigned(1, log2c(number_of_voters_for_one_tdr))));
+    signal sel_data_2 : sel_arr := (others => std_logic_vector(to_unsigned(0, log2c(number_of_voters_for_one_tdr))));
 
     -- per-TDR mux outputs and selected data
     type mux_word_arr is array (0 to num_stages-1) of std_logic_vector(2*input_data_width downto 0);
@@ -69,8 +72,8 @@ architecture Behavioral of mac_triplex_duplex is
     signal error_from_comparator : errcmp_arr := (others => '0');
     -- per-TDR counters sized by voters-per-TDR
     type cnt_arr is array (0 to num_stages-1) of unsigned(log2c(number_of_voters_for_one_tdr)-1 downto 0);
-    signal counter   : cnt_arr := (others => to_unsigned(2, log2c(number_of_voters_for_one_tdr)));
-    constant max_index : unsigned (log2c(number_of_voters_for_one_tdr)-1 downto 0) :=
+    signal counter   : cnt_arr := (others => to_unsigned(1, log2c(number_of_voters_for_one_tdr)));
+    constant max_index : unsigned (log2c(number_of_voters_for_one_tdr+1)-1 downto 0) :=
         to_unsigned(number_of_voters_for_one_tdr-1, log2c(number_of_voters_for_one_tdr));
 
     signal data_outt_s : std_logic_vector (2*input_data_width-1 downto 0) := (others => '0');
@@ -155,19 +158,33 @@ begin
     end generate voter_logic_per_tdr;
 
     -- per-TDR multiplexers: select within the j-th stage voter set
+    
+    -- Prvi MUX prima izlaze svih modula, osim izlaz od drugog modula(1,3,4,5...)
     mux_per_stage:
     for j in 0 to num_stages-1 generate
-        data_from_mux_1(j) <= data_to_switch(j*number_of_voters_for_one_tdr +
-                                             to_integer(unsigned(sel_data_1(j))));
-        data_from_mux_2(j) <= data_to_switch(j*number_of_voters_for_one_tdr +
-                                             to_integer(unsigned(sel_data_2(j))));
+        data_to_switch1(j*number_of_voters_for_one_tdr) <=  data_to_switch(j*number_of_voters_for_one_tdr);
+        assigning_value_for_mux1: 
+        for i in 1 to number_of_voters_for_one_tdr-2 generate
+            data_to_switch1(j*number_of_voters_for_one_tdr+i) <=  data_to_switch(j*number_of_voters_for_one_tdr+i+1);
+        end generate;
+        -- Drugi MUX prima izlaze svih modula, osim izlaz od prvog modula(2,3,4,5...)
+        assigning_value_for_mux2: 
+        for i in 0 to number_of_voters_for_one_tdr-2 generate
+            data_to_switch2(j*number_of_voters_for_one_tdr+i) <=  data_to_switch(j*number_of_voters_for_one_tdr+i+1);
+        end generate;
     end generate;
-
+    
+    mux_per_stage2:
+    for j in 0 to num_stages-1 generate
+        data_from_mux_1(j) <= data_to_switch1(j*number_of_voters_for_one_tdr + to_integer(unsigned(sel_data_1(j))));
+        data_from_mux_2(j) <= data_to_switch2(j*number_of_voters_for_one_tdr + to_integer(unsigned(sel_data_2(j))));
+    end generate;
+    
     --error detection from comparator 
     -- error detection per TDR
     err_detect:
     for j in 0 to num_stages-1 generate
-        process(clk_i)
+        process(clk_i, data_from_mux_1, data_from_mux_2)
         begin
             if rising_edge(clk_i) then
                 if data_from_mux_1(j)(2*input_data_width downto 1) /= data_from_mux_2(j)(2*input_data_width downto 1) then
@@ -186,20 +203,14 @@ begin
         process(clk_i)
         begin
             if rising_edge(clk_i) then
-                if (error_from_comparator(j) = '1' and data_from_mux_1(j)(0) = '1' and sel_data_1(j) /= std_logic_vector(counter(j))) then
+                if ((error_from_comparator(j) = '1' and data_from_mux_1(j)(0) = '1') and sel_data_1(j) /= std_logic_vector(counter(j)) and counter(j) < max_index ) then
                     sel_data_1(j) <= std_logic_vector(counter(j));
-                    if counter(j) = max_index then
-                        counter(j) <= (others => '0');
-                    else
-                        counter(j) <= counter(j) + 1;
-                    end if;
-                elsif (error_from_comparator(j) = '1' and data_from_mux_2(j)(0) = '1' and sel_data_2(j) /= std_logic_vector(counter(j))) then
+                    counter(j) <= counter(j) + 1; 
+                elsif ((error_from_comparator(j) = '1' and data_from_mux_2(j)(0) = '1') and (sel_data_2(j) /= std_logic_vector(counter(j))) and counter(j) < max_index) then
                     sel_data_2(j) <= std_logic_vector(counter(j));
-                    if counter(j) = max_index then
-                        counter(j) <= (others => '0');
-                    else
-                        counter(j) <= counter(j) + 1;
-                    end if;
+                    counter(j) <= counter(j) + 1; 
+                else
+                    counter(j) <= counter(j);
                 end if;
             end if;
         end process;
